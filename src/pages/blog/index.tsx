682c1d52749a56d7c5da101b6b74d391dd79f2c0
talles <talles-guerra@hotmail.com>
2025-06-17 15:18:37 +0100

    first commit

---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,9 @@
+export default function BlogPage() {
+    return (
+        <div>
+            <h2>
+                Blog Page
+            </h2>
+        </div>
+    );
+}
---
+++ src/pages/blog/user/index.tsx
@@ -0,0 +1,9 @@
+export default function UserPage() {
+    return (
+        <div>
+            <h2>
+                User
+            </h2>
+        </div>
+    );
+}
