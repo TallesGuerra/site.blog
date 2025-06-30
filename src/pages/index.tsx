92f86336c3a1397f213e0a28d39b9332402f6755
talles <talles-guerra@hotmail.com>
2025-06-30 14:40:27 +0100

    shadcn ui implemented.
    about, blog and user  folders » Deleted

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 
 const geistSans = Geist({
@@ -14,7 +13,7 @@ const geistMono = Geist_Mono({
 export default function Home() {
   return (
     <div className="min-h-screen min-w-full bg-slate-500 flex flex-col items-center justify-center gap-4 p-4">
-      <h2 className="text-4xl text-green-400">Title 2</h2>
+      <h2 className="text-4xl text-green-500">Title 2</h2>
     </div>
   );
 }
