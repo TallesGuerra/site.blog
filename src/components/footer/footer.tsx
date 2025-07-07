52970eacf2584485c1e99b94cef46fd7c41cd346
talles <talles-guerra@hotmail.com>
2025-07-07 15:04:51 +0100

    footer component created

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,7 +1,31 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
 export const Footer = () => {
     return (
-        <footer>
-            <h2> Footer</h2>
+        <footer className="border-t border-white/10">
+            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                <div className="flex justify-between md:flex-row gap-8 py-8">
+
+                    <Link href="/">
+                        <Image
+                            src="/logo.svg"
+                            alt="Logo"
+                            width={116}
+                            height={32}
+                        />
+                    </Link>
+
+                    <nav className='flex flex-col md:flex-row items-center gap-4 text-sm text-muted-foreground'>
+                        <Link href="/termos-de-uso" className='hover:text-primary'>Termos de uso</Link>
+                        <Link href="/politica-de-privacidade" className='hover:text-primary'>Politica de Privacidade</Link>
+                        <Link href="/feedback" className='hover:text-primary'>Enviar Feedback</Link>
+                    </nav>
+
+
+                </div>
+            </div>
+
         </footer>
     )
 }
